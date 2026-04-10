28ffc0f45ee75f58f3fbde1d7d57882b803665cb
권민규 [kmgmg2391601] <kmgmg2391601@nexon.co.kr>
2026-04-10 11:17:33 +0900

    remove sci fi ui.

---
+++ Source/DDD/GameMode/DDDGameplayMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DDDGameplayMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DDD_API ADDDGameplayMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
